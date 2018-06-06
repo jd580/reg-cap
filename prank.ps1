e98caa3dabcd9ea58e2ed236c546226dfceaf237
coffeecoder3peat <philip.c.wilson@outlook.com>
2018-06-05 21:32:37 -0500

    update for shorter var names

--- prank.ps1
+++ prank.ps1
@@ -1,7 +1,7 @@
 # If you have enough time to type it in on a logged on computer
 # type "cls" (clear-host) and leave the window open to get rid of the proof but let it keep running
 # Also, I'm childish. ;D press F5 to run and ctrl+c to stop 
-# also if you have a lot of time, you could wrap it in a timer to set it off unexpectedly
+# also if you have a lot of time, you could wrap it in a timer to set it off unexpectedlyc
 
 while ($true)
 {
@@ -10,4 +10,4 @@ while ($true)
 
     [console]::beep($tone,$length)
 }
-cls
+

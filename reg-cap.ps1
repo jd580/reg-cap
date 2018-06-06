af6972f453ff50b235e28c74d3c7ce18f5cbd97c
JD <jakedouglascrandell@gmail.com>
2018-06-05 20:19:43 -0700

    Win Env Info

--- reg-cap.ps1
+++ reg-cap.ps1
@@ -2,5 +2,20 @@
 Write-Output "Windows Environment Info"
 Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System' #grab system hardware info
 Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control' #params that control system startup
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\ComputerName'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\CrashControl'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\hivelist'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\FileRenameOperations'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\KnownDLLs'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Memory Management'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug'
+#Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Hot Fix' #not in win10
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages'
+Get-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+
 
 

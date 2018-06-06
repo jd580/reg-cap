4dc12a10e1850736e449ce1bd0e15ad6f6d95d62
JD <jakedouglascrandell@gmail.com>
2018-06-05 18:51:59 -0700

    test commit2

--- test.ps1
+++ test.ps1
@@ -1,2 +1,3 @@
-Write-Output "test"
-Write-Output "22"
+#Capture relevant reg keys for forensic details
+Get-Item -path 'registry::HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System'
+Get-Item -path 'registry::HKEY_LOCAL_MACHINE\system\CurrentControlSet\Control'
